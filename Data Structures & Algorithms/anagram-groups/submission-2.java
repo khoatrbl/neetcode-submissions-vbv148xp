fa92ffc207190d5a32357ca6ba3c1d1e12510b17
Khoa Nguyen <73837315+khoatrbl@users.noreply.github.com>
2026-04-09 23:55:44 +0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            String countKey = Arrays.toString(count);
+            res.putIfAbsent(countKey, new ArrayList<>());
+            res.get(countKey).add(s);
+        }
+
+        
+
+        return new ArrayList<>(res.values());
+    }
+}
