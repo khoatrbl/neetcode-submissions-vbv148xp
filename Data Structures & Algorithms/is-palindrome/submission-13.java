4b3018e9b1b2a665e1d06c453d17f42e31ec6901
Khoa Nguyen <73837315+khoatrbl@users.noreply.github.com>
2026-04-16 17:44:36 +0700

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left <= right) {
+            if (!isAlphaNumeric(s.charAt(left))) {
+                left++;
+                continue;
+            }
+
+            if (!isAlphaNumeric(s.charAt(right))) {
+                right--;
+                continue;
+            }
+
+            if (s.toLowerCase().charAt(left) != s.toLowerCase().charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return c >= 'a' && c <= 'z' 
+            || c >= 'A' && c <= 'Z' 
+            || c >= '0' && c <= '9';
+    }
+}
