4e9bdc3435e839d92247e444917a322449554388
Khoa Nguyen <73837315+khoatrbl@users.noreply.github.com>
2026-04-22 20:55:03 +0700

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // calculate prefix of every index
+        // calculate suffix of every index
+
+        int[] res = new int[nums.length];
+        res[0] = 1;
+
+        int prefix = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            prefix *= nums[i];
+
+            res[i + 1] = prefix;
+        }
+
+        System.out.println(Arrays.toString(res));
+
+        int suffix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] = suffix * res[i];
+            suffix *= nums[i];
+        }
+
+        return res;
+    }
+}  
