6bf00f02681bf40d9a8a780c23a622ff2bb63d4d
Khoa Nguyen <73837315+khoatrbl@users.noreply.github.com>
2026-04-22 15:14:42 +0700

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+
+        if (s.length() == 1) {
+            return 1;
+        }
+
+        Set<Character> seen = new HashSet<>();
+
+        int l = 0;
+        int longest = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            while (seen.contains(s.charAt(r))) {
+                seen.remove(s.charAt(l));
+                l++;
+            }
+            seen.add(s.charAt(r));
+            longest = Math.max(longest, (r - l + 1));
+        }
+
+        return longest;
+        
+    }
+}
