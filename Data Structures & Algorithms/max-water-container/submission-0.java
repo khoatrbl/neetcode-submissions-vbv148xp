6825270ee28e9684bd105febed409bb90a44c610
Khoa Nguyen <73837315+khoatrbl@users.noreply.github.com>
2026-04-22 14:36:32 +0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        // BRUTE FORCE APPROACH
+        int res = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = i + 1; j < heights.length; j++) {
+                int area = (j - i) * Math.min(heights[i], heights[j]);
+                res = Math.max(area, res);
+            }
+        }
+
+        return res;
+    }
+
+    
+}
