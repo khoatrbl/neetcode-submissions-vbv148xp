5b72ae4e3c5e0ef258c1724a76e5e68e261324cc
Khoa Nguyen <73837315+khoatrbl@users.noreply.github.com>
2026-04-23 16:35:17 +0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,32 @@
+class MinStack {
+    Stack<Integer> values;
+    Stack<Integer> minimum;
+
+    public MinStack() {
+        this.values = new Stack();
+        this.minimum = new Stack();    
+    }
+    
+    public void push(int val) {
+        this.values.push(val);
+
+        if (this.minimum.isEmpty()) {
+            minimum.push(val);
+        } else {
+            minimum.push(Math.min(minimum.peek(), val));
+        }
+    }
+    
+    public void pop() {
+        this.values.pop();
+        this.minimum.pop();
+    }
+    
+    public int top() {
+        return this.values.peek();
+    }
+    
+    public int getMin() {
+        return this.minimum.peek();
+    }
+}
