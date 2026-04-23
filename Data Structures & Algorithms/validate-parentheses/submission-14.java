c2b32faa62b9af6d9df44da53e233e9d07315145
Khoa Nguyen <73837315+khoatrbl@users.noreply.github.com>
2026-04-23 16:04:34 +0700

    Add: validate-parentheses - submission-14

---
+++ Data Structures & Algorithms/validate-parentheses/submission-14.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> pairs = new HashMap<>();
+        pairs.put(')','(');
+        pairs.put(']','[');
+        pairs.put('}','{');
+
+        Stack<Character> stack = new Stack();
+
+        for (char c : s.toCharArray()) {
+            if (!pairs.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty() && stack.peek() == pairs.get(c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
