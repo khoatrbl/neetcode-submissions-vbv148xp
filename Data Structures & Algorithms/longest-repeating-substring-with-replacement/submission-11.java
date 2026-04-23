dad64d2474fd771b51b121edc0f18fa459dfc811
Khoa Nguyen <73837315+khoatrbl@users.noreply.github.com>
2026-04-24 02:38:08 +0700

    Add: longest-repeating-substring-with-replacement - submission-11

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-11.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int res = 0;
+
+        int l = 0;
+        int maxF = 0;
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (int r = 0; r < s.length(); r++) {
+            count.put(s.charAt(r), count.getOrDefault(s.charAt(r), 0) + 1);
+            maxF = Math.max(maxF, count.get(s.charAt(r)));
+
+            while (( r - l + 1) - maxF > k) {
+                count.put(s.charAt(l), count.get(s.charAt(l)) - 1);
+                l++;
+            }
+
+            res = Math.max(res,  r - l + 1);
+        }
+
+        return res;
+    }
+}
